efc7e3a9008bb42e6e90e4daccef7d7a1f26ff0d
melinaruleri <melinajruleri@gmail.com>
2025-10-17 19:21:00 -0300

    hice la parte de editar, modificar y eliminar

--- router.php
+++ router.php
@@ -37,7 +37,8 @@ switch ($params[0]) {
         break;
     case 'librosPorAutor':
         $controller = new ControladorAutor();
-        $controller->showAutores($params[1]);
+        // cambiamos a showAutores para que use el método que lista libros por autor
+        $controller->showDetalleAutor($params[1]);
         break;
     case 'login': 
         $controller = new ControladorSeguridad();
@@ -46,6 +47,30 @@ switch ($params[0]) {
     case 'logout': // (B) - FUNCIONAL
         $controller = new ControladorSeguridad(); 
         $controller->logout();
+        break;
+    case 'agregarAutorForm': // Muestra el formulario de Alta (GET)
+        $controller = new ControladorAutor();
+        $controller->showFormAgregarAutor();
+        break;
+        
+    case 'agregarAutor': // Procesa la creación (POST)
+        $controller = new ControladorAutor();
+        $controller->agregarAutor();
+        break;
+        
+    case 'editarAutorForm': // Muestra el formulario de Edición (GET /id)
+        $controller = new ControladorAutor();
+        $controller->showFormEditarAutor($params[1]);
+        break;
+        
+    case 'editarAutor': // Procesa la edición (POST)
+        $controller = new ControladorAutor();
+        $controller->editarAutor();
+        break;
+        
+    case 'eliminarAutor': // Procesa la eliminación (GET /id)
+        $controller = new ControladorAutor();
+        $controller->eliminarAutor($params[1]);
         break;
 
     default:
